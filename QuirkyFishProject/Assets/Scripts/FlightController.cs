a434c05370044e66c021bee9a4c4593b76d5c369
loreleibasken <lbasken@student.dwc.edu>
2017-09-13 23:03:07 -0400

    Added movement and constraints

---
+++ QuirkyFishProject/Assets/Scripts/FlightController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightController : MonoBehaviour {
+
+    public float FlightForce = 75.0f;
+    public Rigidbody2D rb;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        bool flightActive = Input.GetKey(KeyCode.W);
+
+        if (flightActive)
+        {
+            rb.AddForce(new Vector2(0, FlightForce));
+        }
+		
+	}
+}
