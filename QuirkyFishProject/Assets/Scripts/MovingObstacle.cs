4a5d3bf3fab5e70949e5a14a4d9eb6342f6a6c26
Binh <binjaaaa@gmail.com>
2017-09-17 16:08:13 -0500

    moving bird script, and timer ui

---
+++ QuirkyFishProject/Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+
+    public GameObject Obstacle;
+    public Rigidbody2D myRigi;
+    //point A
+    private Vector3 pos1 = new Vector3(-4, 0, 0);
+    //point B
+    private Vector3 pos2 = new Vector3(4, 0, 0);
+    //How Fast
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the object to and from two set points at a certain speed
+        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+
+
+    //make a fall function cause gravity is too much for these bois
+
+    //Collision with the player will destroy the player object
+    private void OnCollisionEnter2D(Collision2D aCol)
+    {
+        if (aCol.collider.tag == "Player")
+        {
+            Destroy(aCol.gameObject);
+        }
+    }
+}
---
+++ QuirkyFishProject/Assets/Scripts/TimerScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class TimerScore : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //time starts up once the application starts up
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //time since the application starts up
+        float t = Time.time - startTime;
+        //converting the float of Time.time into an int that we can
+        //output to the text UI in meters
+        string meters = (t).ToString("f0");
+        
+        //outputting the seconds as meters
+        timerText.text = meters + "m";
+	}
+}
