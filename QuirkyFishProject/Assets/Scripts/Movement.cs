7208cc6fcf0b034fe1f8af0b7be11d8772b37ae1
loreleibasken <lbasken@student.dwc.edu>
2017-09-17 22:37:28 -0400

    added shield script and changed movement script to handle horizontal and vertical

--- QuirkyFishProject/Assets/Scripts/Movement.cs
+++ QuirkyFishProject/Assets/Scripts/Movement.cs
@@ -8,38 +8,10 @@ public class Movement : MonoBehaviour {
     public Rigidbody2D rb;
 
     void Start () {
-		
 	}
-	
 	
 	void FixedUpdate ()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(new Vector2(-FlightForce, 0));
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(new Vector2(FlightForce, 0));
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            /*
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-            */
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            /*
-            Vector3 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-            */
-        }
-
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * FlightForce, Input.GetAxis("Vertical") * FlightForce));
     }
 }
---
+++ QuirkyFishProject/Assets/Scripts/Shield.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public float maxDurationSec;
+
+    private float timeElapsed = 0;
+    private bool shieldOn = false;
+    
+    void setShield (bool on)
+    {
+        if (shieldOn != on)
+        {
+            shieldOn = on;
+            if (shieldOn)
+            {
+                Debug.Log("shield ON");
+                // enables shield
+            }
+            else
+            {
+                Debug.Log("shield OFF");
+                // disable shield
+            }
+        }
+    }
+	
+	void Start ()
+    {
+	}
+		
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            if (timeElapsed < maxDurationSec)
+            {
+                setShield(true);
+                timeElapsed += Time.deltaTime;
+            }
+            else
+            {
+                setShield(false);
+            }
+
+        } else 
+        {
+            setShield(false);
+        }
+	}
+}
